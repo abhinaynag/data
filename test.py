d0f2c4fbc7a30916d6a2da2210f29e9333c7978d
Abhinay Nag <abhinaynag@gmail.com>
2017-09-25 06:40:04 +1300

    test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import re, sys , os , string
 
-fh= open('splunk_raw_log','r')
-#sig =" * "+"\n * ".join(list(set(re.findall("snort:\s\[1:\d+:\d+\].*{\w+}",fh.read()))))
-#sip =' , '.join(set(re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ','.join(re.findall("\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+\s->)",fh.read())))))
-#xfwd=' , '.join(re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",repr(set(re.findall("(\d{2}\/\d{2}\/\d{2,4})\s(\d{2}:\d{2}:\d{2})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",fh.read())))))
-#sport=string.replace(', '.join(set(re.findall(":\d+\s" , repr(re.findall("\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+\s->)",fh.read()))))),":","")
-#dip= ' , '.join(set(re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",repr(re.findall("->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):\d+",fh.read())))))
-#dport=string.replace(' , '.join(set(re.findall(":\d+" , repr(re.findall("->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+)",fh.read()))))),":","")
+fh= open('raw','r')
+data=fh.read()
 
-print dport
+print "\n \n ******* Start of Output  **************"
+#sig =" * "+"\n * ".join(set(re.findall("(snort:\s\[1:\d+:\d+\].*:\s\d{1}\])",data)))
+#sig =list((set(re.findall("((snort:\s\[1:\d+:\d+\](\s\w+)+(\s\((\w+\W?)+\))?(\s\[Classification:(\s\w+)+\])(\s\[Priority:\s\d{1}\])\s{\w{3}}))",data))))
+tmp= repr(list(set(re.findall("((snort:\s\[1:\d+:\d+\](\s\w+)+(\s\((\w+\W?)+\))?(\s\[Classification:(\s\w+)+\])(\s\[Priority:\s\d{1}\])\s{\w{3}}))",data))))
+for item in tmp:
+
+#sip =' , '.join(set(re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", ','.join(re.findall("\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+)\s->",data)))))
+#xfwd=' , '.join(re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",repr(set(re.findall("(\d{2}\/\d{2}\/\d{2,4})\s+(\d{2}:\d{2}:\d{2})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",data)))))
+#sport=string.replace(', '.join(set(re.findall(":\d+\s" , repr(re.findall("\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+\s->)",data))))),":","")
+#dip= ' , '.join(set(re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",repr(re.findall("->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):\d+",data)))))
+#dport=string.replace(' , '.join(set(re.findall(":\d+" , repr(re.findall("->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+)",data))))),":","")
+fh.close()
+print sig
+#print sig+"\n"+sip+"\n"+xfwd+"\n"+sport+"\n"+dip+"\n"+dport
+
